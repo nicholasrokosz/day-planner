c4af6062979c19902a36d14bcb41da1282561450
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2020-11-12 09:45:23 -0700

    add localStorage functionality

--- app.js
+++ app.js
@@ -1,36 +1,56 @@
-for(let hour = 9; hour < 18; hour++) {
-    $(".container").append(createTimeSlot(hour));
+for (let hour = 9; hour < 18; hour++) {
+  $(".container").append(createTimeSlot(hour));
 }
 function createTimeSlot(hour) {
   const $timeSlot = $("<div>")
-  .attr("id", `hour-${hour}`)
-  .attr("value", hour)
-  .attr("class", "row time-block");
+    .attr("id", `hour-${hour}`)
+    .attr("value", hour)
+    .attr("class", "row time-block");
   const $timeLabel = $("<div>").attr("class", "col-md-1 hour");
   if (hour > 12) {
     $timeLabel.text(`${hour - 12} PM`);
   } else if (hour === 12) {
     $timeLabel.text(`${hour} PM`);
   } else {
-      $timeLabel.text(`${hour} AM`);
+    $timeLabel.text(`${hour} AM`);
   }
-  const $textArea = $("<textarea>")
-  .attr("class", "col-md-10 description");
+  const $textArea = $("<textarea>").attr("class", "col-md-10 description")
   const $saveBtn = $("<button>")
-  .attr("class", "btn saveBtn col-md-1")
-  .append($("<i>").attr("class", "fas fa-save"));
-  $timeSlot.append($timeLabel, $textArea, $saveBtn);
-  
-  let currentTime = luxon.DateTime.local().toFormat("H");
-  $(".container").each(function(block){
-      console.log($timeSlot.attr("value"));
-      if($timeSlot.attr("value") < currentTime) {
-          $textArea.css("background-color", "gray");
-          } else if ($timeSlot.attr("value") == currentTime){
-            $textArea.css("background-color", "red");
-          } else{
-            $textArea.css("background-color", "green");
-          }
+    .attr("class", "btn saveBtn col-md-1")
+    .append($("<i>").attr("class", "fas fa-save"));
+    $timeSlot.append($timeLabel, $textArea, $saveBtn);
+    
+  let currentTime = parseInt(luxon.DateTime.local().toFormat("H"));
+  $(".container").each(function() {
+    //console.log($timeSlot.attr("value"));
+    if ($timeSlot.attr("value") < currentTime) {
+      $textArea.css("background-color", "gray");
+    } else if ($timeSlot.attr("value") == currentTime) {
+      $textArea.css("background-color", "red");
+    } else {
+      $textArea.css("background-color", "green");
+    }
   });
   return $timeSlot;
 }
+
+
+let plansForDay = localStorage.getItem("plansForDay");
+if (plansForDay){
+    plansForDay = JSON.parse(plansForDay);
+} else {
+    plansForDay = [];
+}
+$(".saveBtn").click( function(e){
+    plansForDay.push({
+        currentHour: $(this).siblings('.hour').text().split(' ')[0],
+        currentText: $(this).siblings('textarea').val() // <-- 
+    });
+    localStorage.setItem("plansForDay", JSON.stringify(plansForDay));
+})
+
+console.log(plansForDay);
+
+plansForDay.forEach(function(plan) {
+  $("div[value='" + plan.currentHour + "']").children('textarea').text(plan.currentText);
+})
